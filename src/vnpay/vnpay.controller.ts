a6452b6d98801ecc3db8172f6bb78280f6bcf9fd
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-17 21:32:28 +0700

    update

--- src/vnpay/vnpay.controller.ts
+++ src/vnpay/vnpay.controller.ts
@@ -12,13 +12,5 @@ import { VnPayService } from './vnpay.service';
 
 @Controller('order')
 export class VnPayController {
-  constructor(private vnpayServie: VnPayService) {}
-  @Post('/create_payment_url')
-  createVnpay(@Req() req: Request, @Res() res: Response) {
-    return this.vnpayServie.createVnpay(req, res);
-  }
-  @Get('/vnpay_return')
-  vnpayReturn(@Req() req: Request, @Res() res: Response) {
-    return this.vnpayServie.vnpayReturn(req, res);
-  }
+  
 }
