67e11c12ee8522eb144f257afb2238040fcf4055
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-17 21:39:18 +0700

    update

--- src/vouchers/dto/vouchers.dto.ts
+++ src/vouchers/dto/vouchers.dto.ts
@@ -1,19 +1,7 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class VouchersDto {
-  @IsString()
-  @IsNotEmpty()
-  readonly code: string;
-  @IsString()
-  readonly description: string;
-  @IsString()
-  readonly end_date: string;
-  @IsString()
-  readonly start_date: string;
-  @IsString()
-  readonly discount_type: string;
-  @IsNumber()
-  readonly discount_value: number;
+  
 }
 
 export class idVouchersDto {
