d175600da2ed7dafa9bb59fec8c3b413d93cce78
diepngng1007 <125068873+diepngng1007@users.noreply.github.com>
2024-07-17 21:02:56 +0700

    update

--- src/vietqr/vietqr.controller.ts
+++ src/vietqr/vietqr.controller.ts
@@ -12,5 +12,14 @@ import { VietqrService } from './vietqr.service';
 
 @Controller('qr_code')
 export class VietQrController {
+  constructor(private vietqrServie: VietqrService) {}
+  @Post('/generate-qr')
+  createQrCode(@Req() req: Request, @Res() res: Response) {
+    return this.vietqrServie.generateQrCode(req, res);
+  }
 
+  @Get('/banks')
+  getBanks(@Req() req: Request, @Res() res: Response) {
+    return this.vietqrServie.getBank( res);
+  }
 }
