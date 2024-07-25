0c129d51fed97b8cb24a914ad5c18799d5b306be
danglhph30148 <danglhph30148@fpt.edu.vn>
2024-07-25 17:04:20 +0700

    lesson dto

---
+++ src/lesson/dto/lesson.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class LessonDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly title: string;
+  @IsString()
+  readonly description: string;
+  @IsNumber()
+  readonly duration: number;
+  @IsArray()
+  readonly sub_lesson: [string];
+  @IsArray()
+  readonly courses_id: [string];
+}
