5de42b56a8d25583e63a044d738ee325643391e6
Duyet11 <114544482+Duyet11@users.noreply.github.com>
2024-07-25 22:50:59 +0700

    Fix permission schema

--- src/permission/schema/permission.schema.ts
+++ src/permission/schema/permission.schema.ts
@@ -1,4 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
 
 
 @Schema({ timestamps: true, versionKey: false })
