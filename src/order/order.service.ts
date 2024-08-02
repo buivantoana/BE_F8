1c8f338873b4b246c554fdeb2b7275b5f5e258e7
Duyet11 <114544482+Duyet11@users.noreply.github.com>
2024-08-02 07:59:56 +0700

    Update orderService

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -29,8 +29,29 @@ export class OrderService {
       console.log(error);
     }
   }
-  //update
-  
+  async updateOrder(id: string) {
+    try {
+      console.log(id);
+      let data = await this.orderModel.findOneAndUpdate(
+        { _id: id },
+        { $set: { status: true } },
+        { returnOriginal: false },
+      );
+      if (!data) {
+        return {
+          status: 1,
+          message: 'failed',
+        };
+      }
+      return {
+        status: 0,
+        message: 'suceess',
+        data,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
   async deleteOrder(id: string) {
     try {
       let data = await this.orderModel.findByIdAndDelete(id);
@@ -91,6 +112,22 @@ export class OrderService {
       console.log(error);
     }
   }
-
-  
+  async findUserOrderAndCourese(courses_id: string,user_id:string) {
+    try {
+      let data = await this.orderModel.find({courses_id:[courses_id],user_id:[user_id]});
+      if (!data) {
+        return {
+          status: 1,
+          message: 'failed',
+        };
+      }
+      return {
+        status: 0,
+        message: 'suceess',
+        data,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
