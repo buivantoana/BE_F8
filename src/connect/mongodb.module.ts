b83310acb1f1b42c8407a3137592397bb5d4bb47
anhtucao <caoanhtu611@gmail.com>
2024-08-27 09:37:39 +0700

    url mongo local

--- src/connect/mongodb.module.ts
+++ src/connect/mongodb.module.ts
@@ -3,11 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb://localhost:27017/datn',
-    ),
-  ],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/datn')],
 })
 export class MongodbConnect implements OnModuleInit {
   async onModuleInit() {
